9f108a4ea478de7c52d79f246a85442dec577707
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-06-17 05:46:51 -0700

    Added the Header Box.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import React from "react";
 
 const Home = () => {
+  const loggedIn = { firstName: "Hassan" };
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="username"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access account manage it and perform transactions"
+          />
+        </header>
+      </div>
+    </section>
   );
 };
 
