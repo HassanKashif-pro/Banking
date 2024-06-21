70404542d8fe9b90e6e1cd43a326e351cf87e6e9
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-06-21 23:23:41 +0500

    Added the routing for pages (preventing 404). && Implemented the Complete Layout

---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TransactionHistory = () => {
+  return <div></div>;
+};
+
+export default TransactionHistory;
---
+++ app/(root)/transfer-history/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Trasfer = () => {
+  return <div></div>;
+};
+
+export default Trasfer;
