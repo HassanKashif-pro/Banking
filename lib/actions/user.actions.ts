50128d2ac59a21fa997a72d1bff4b68801524711
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-06-27 19:45:30 +0500

    Added new user action "getLoggedInUser" and customized the signup.

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,11 @@
 "use server";
 
+import { cookies } from "next/headers";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { parse } from "path";
+import { parseStringify } from "../utils";
+import { ID } from "node-appwrite";
+
 export const signIn = async () => {
   try {
   } catch (error) {
@@ -8,8 +14,37 @@ export const signIn = async () => {
 };
 
 export const signUp = async (userData: SignUpParams) => {
+  const { email, password, firstName, lastName } = userData;
   try {
+    const { account, database } = await createAdminClient();
+
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      `${firstName} ${lastName}`
+    );
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify(newUserAccount);
   } catch (error) {
     console.error("Error", error);
   }
 };
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
