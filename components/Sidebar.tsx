cf5320f6ac77700f3ebe3622edb5470eac9d5508
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-06-29 14:22:05 +0500

    Added Additional info in the footer in sideBar.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Footer from "./Footer";
 const Sidebar = ({ user }: SidebarProps) => {
   const pathname = usePathname();
 
@@ -49,6 +50,8 @@ const Sidebar = ({ user }: SidebarProps) => {
           );
         })}
       </nav>
+
+      <Footer user={user} />
     </section>
   );
 };
