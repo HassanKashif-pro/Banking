03ca7424bc62a5d65208c9b9b9b95b2e85ea35e6
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-07-03 16:10:49 +0500

    added a user.action "createBankAccount" for Dwolla

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -6,6 +6,10 @@ import {
   usePlaidLink,
 } from "react-plaid-link";
 import { useRouter } from "next/navigation";
+import {
+  createLinkToken,
+  exchangePublicToken,
+} from "@/lib/actions/user.actions";
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
   const router = useRouter();
@@ -14,8 +18,8 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
 
   useEffect(() => {
     const getLinkToken = async () => {
-      //const data = await createLinkToken(user);
-      // setToken(data?.linkToken);
+      const data = await createLinkToken(user);
+      setToken(data?.linkToken);
     };
 
     getLinkToken();
@@ -23,10 +27,10 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
 
   const onSuccess = useCallback<PlaidLinkOnSuccess>(
     async (public_token: string) => {
-      // await exchangePublicToken({
-      //   publicToken: public_token,
-      //   user,
-      // })
+      await exchangePublicToken({
+        publicToken: public_token,
+        user,
+      });
 
       router.push("/");
     },
